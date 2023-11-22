2e968d3b0b58dfca0a8f9c6bb375fed92db3655b
AKP03 <82044012+AKP03@users.noreply.github.com>
2023-11-22 13:12:10 +0530

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,7 @@
+const CLIENT_ID='896271094563-v5srq9drf9fsfrjkhsruh8c02m20767c.apps.googleusercontent.com';
+const CLIENT_SECRET='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';
+const REDIRECT_URI='https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN='1//04a37xE8D-M9gCgYIARAAGAQSNwF-L9IrGtU4OIOcJ8isgIqPfUlIBJ_oPQXUVCiantYHcoUrNZ-m__iYJPJ0whIdNxcf3WVjJJE';
+module.exports ={CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,REFRESH_TOKEN};
+
+
